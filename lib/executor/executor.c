22d6250ddaaf192b1054c4ee80ea9c695ac052ec
Fenna Snelderwaard <fsnelder@student.codam.nl>
2022-12-08 13:51:53 +0100

    executor implementation

--- lib/executor/executor.c
+++ lib/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: fsnelder <fsnelder@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/12/07 15:17:55 by fsnelder      #+#    #+#                 */
-/*   Updated: 2022/12/08 11:28:38 by fsnelder      ########   odam.nl         */
+/*   Updated: 2022/12/08 13:49:01 by fsnelder      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ static void	executor_init(t_executor *executor, t_list *commands)
 	executor->ncommands = ft_lstsize(commands);
 	executor->commands = commands;
 	executor->pids = (int *)ft_malloc(executor->ncommands * sizeof(int));
+	ft_memset(executor->pids, -1, sizeof(int) * executor->ncommands);
 }
 
 static void	executor_destroy(t_executor *executor)
@@ -37,6 +38,8 @@ static void	executor_wait(t_executor *executor)
 	i = 0;
 	while (i < (int)executor->ncommands)
 	{
+		if (executor->pids[i] == -1)
+			break ;
 		// TODO: error handling and exit code
 		waitpid(executor->pids[i], NULL, 0);
 		i++;
@@ -59,14 +62,52 @@ void	expand_redirection(void *ptr)
 	redirect->expanded = expand_token(redirect->word, (const char **)environ);
 }
 
+static char	*construct_full_path(const char *directory, const char *name)
+{
+	char	*temp;
+	char	*full_path;
+
+	temp = ft_strjoin(directory, "/");
+	full_path = ft_strjoin(temp, name);
+	free(temp);
+	return (full_path);
+}
+
+static char	*search_path(const char *path, const char *name)
+{
+	char	**paths;
+	int		i;
+	char	*full_path;
+
+	paths = ft_split(path, ':');
+	i = 0;
+	while (paths[i] != NULL)
+	{
+		full_path = construct_full_path(paths[i], name);
+		if (access(full_path, X_OK) == 0)
+			return (full_path);
+		free(full_path);
+		i++;
+	}
+	return (NULL);
+}
+
 // function finds the command based on the PATH variable
 // if a '/' does not appear
 static int	find_command(char **out_path, char *name)
 {
+	const char	*path;
+
 	*out_path = name;
 	if (ft_strchr(name, '/'))
 		return (SUCCESS);
 	// TODO: search in PATH
+	path = getenv("PATH");
+	if (!path)
+		return (COMMAND_NOT_FOUND);
+	*out_path = search_path(path, name);
+	if (!*out_path)
+		return (COMMAND_NOT_FOUND);
 	return (SUCCESS);
 }
 
@@ -78,6 +119,7 @@ int	set_redirect(t_redirect *redirect, int file_mode, int dup_to)
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	if (fd < 0)
 	{
+		// TODO: use filename for error message
 		perror("minishell: open");
 		return (GENERAL_ERROR);
 	}
@@ -149,7 +191,7 @@ char	**list_arguments_to_array(t_list *arguments)
 5. execute the command
 */
 
-static int	handle_child_process(t_executor *executor, t_command *command)
+static int	handle_child_process(t_command *command)
 {
 	t_list	*expanded_arguments;
 	int		result;
@@ -166,7 +208,8 @@ static int	handle_child_process(t_executor *executor, t_command *command)
 	if (set_redirections(command->redirections) != SUCCESS)
 		return (GENERAL_ERROR);
 	args = list_arguments_to_array(expanded_arguments);
-	execve(args[0], args, environ);
+	execve(full_path, args, environ);
+	perror("minishell");
 	return (GENERAL_ERROR);
 }
 
@@ -176,10 +219,80 @@ static int	execute_one(t_executor *executor, int index, t_command *command)
 
 	pid = fork();
 	if (pid == 0)
-		exit(handle_child_process(executor, command));
+		exit(handle_child_process(command));
 	else if (pid < 0)
 		return (GENERAL_ERROR);
 	executor->pids[index] = pid;
+	return (SUCCESS);
+}
+
+static int execute_piped_child(int std_in, int std_out, t_command *command)
+{
+	if (std_in != -1)
+		dup2(std_in, STDIN_FILENO);
+	if (std_out != -1)
+		dup2(std_out, STDOUT_FILENO);
+	close(std_in);
+	close(std_out);
+	exit(handle_child_process(command));
+}
+
+typedef struct s_command_info
+{
+	int			std_in;
+	int			std_out;
+	t_command	*command;
+}	t_command_info;
+
+static t_command_info	make_command_info(
+	int std_in, int std_out, t_command *command)
+{
+	return ((t_command_info){std_in, std_out, command});
+}
+
+// 1. fork
+// 2. redirect STDIN (if not first process)
+// 3. redirect STDOUT (if not last process)
+static int	execute_piped_command(
+	t_executor *executor, int index, t_command_info info)
+{
+	int	pid;
+
+	pid = fork();
+	if (pid == 0)
+		exit(execute_piped_child(info.std_in, info.std_out, info.command));
+	else if (pid < 0)
+		return (GENERAL_ERROR);
+	executor->pids[index] = pid;
+	close(info.std_in);
+	close(info.std_out);
+	return (SUCCESS);
+}
+
+static int	execute_pipe_sequence(t_executor *executor, t_list *commands)
+{
+	int					fds[2];
+	int					previous_fd;
+	int					index;
+
+	index = 0;
+	previous_fd = -1;
+	while (commands)
+	{
+		if (commands->next == NULL)
+			return (execute_piped_command(
+					executor, index, make_command_info(
+						previous_fd, -1, (t_command *)commands->content)));
+		if (pipe(fds) < 0)
+			return (GENERAL_ERROR);
+		if (execute_piped_command(
+				executor, index, make_command_info(previous_fd, fds[1],
+					(t_command *)commands->content)) != SUCCESS)
+			return (GENERAL_ERROR);
+		previous_fd = fds[0];
+		index++;
+		commands = commands->next;
+	}
 	return (SUCCESS);
 }
 
@@ -193,10 +306,9 @@ int	execute(t_list *commands)
 	if (executor.ncommands == 1)
 		result = execute_one(&executor, 0, (t_command *)commands->content);
 	else
-		abort();
-		// return (execute_pipe_sequence(commands));
-	if (result != SUCCESS)
-		return (result);
+		result = execute_pipe_sequence(&executor, commands);
+	// TODO: reconsider: kill other child processes if a system call error occurs?
 	executor_wait(&executor);
+	executor_destroy(&executor);
 	return (result);
 }
